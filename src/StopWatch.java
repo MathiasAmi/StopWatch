966a47e2433bc6a0009ed8a0f9a012eca606918d
MathiasAmi <math35i0@edu.easj.dk>
2018-10-29 13:08:00 +0100

    Initial commit

---
+++ src/StopWatch.java
@@ -0,0 +1,35 @@
+public class StopWatch {
+    public StopWatch(){
+        long startTime = System.currentTimeMillis();
+        long endTime = System.currentTimeMillis();
+    }
+
+    private long startTime = System.currentTimeMillis();
+    private long endTime = System.currentTimeMillis();
+    public void start(){
+        startTime = System.currentTimeMillis();
+    }
+    public void stop(){
+        endTime = System.currentTimeMillis();
+    }
+    public long getElapsedTime(){
+        long elapsedTime = endTime - startTime;
+        return elapsedTime;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
+    }
+}
